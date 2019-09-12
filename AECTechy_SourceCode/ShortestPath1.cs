748d9337a8fbd7fdfe8f63215784a28b52f5e714
JUNHAN <junhanloh93@gmail.com>
2019-09-13 00:04:53 +0800

    Added ShortestPath

---
+++ AECTechy_SourceCode/ShortestPath1.cs
@@ -0,0 +1,51 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Analysis;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+#endregion
+
+namespace AECTechy_SourceCode
+{
+    [Transaction(TransactionMode.Manual)]
+    class ShortestPath1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            
+            //Select first room
+            Reference selection = uidoc.Selection.PickObject( ObjectType.Element);      //Select Element 1
+            ElementId eleId = selection.ElementId;
+            LocationPoint eleLocPt = doc.GetElement(eleId).Location as LocationPoint;
+            XYZ roomXyzPoint1 = eleLocPt.Point;
+
+            //Select second room
+            Reference selection2 = uidoc.Selection.PickObject( ObjectType.Element);     //Select Element 2
+            ElementId eleId2 = selection2.ElementId;
+            LocationPoint eleLocPt2 = doc.GetElement(eleId2).Location as LocationPoint;
+            XYZ roomXyzPoint2 = eleLocPt2.Point;
+
+            //Find shortest Path
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Find shortest Path !");
+
+                PathOfTravel route = PathOfTravel.Create( doc.ActiveView, roomXyzPoint1, roomXyzPoint2);
+                
+                tx.Commit();
+            }
+            
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ AECTechy_SourceCode/ShortestPath2.cs
@@ -0,0 +1,53 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Analysis;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+#endregion
+
+namespace AECTechy_SourceCode
+{
+    [Transaction(TransactionMode.Manual)]
+    class ShortestPath2 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            IList<Reference> selection1 = uidoc.Selection.PickObjects(ObjectType.Element);      //Select 1st list
+            IList<XYZ> roomsXyz1 = selection1                                                   //Room 1,2 & 3
+                .Select(r => doc.GetElement(r.ElementId).Location)
+                .Cast<LocationPoint>()
+                .Select(locPoint => locPoint.Point)
+                .ToList();
+
+            IList<Reference> selection2 = uidoc.Selection.PickObjects( ObjectType.Element );    //Select 2nd list
+            IList<XYZ> roomsXyz2 = selection2                                                   //Room 4,6 & 8
+                .Select(r => doc.GetElement(r.ElementId).Location)                              
+                .Cast<LocationPoint>()
+                .Select(locPoint => locPoint.Point)
+                .ToList();
+
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Find shortest Path !");
+
+                //IList<PathOfTravel> routeMap = PathOfTravel.CreateMapped(doc.ActiveView, roomsXyz1, roomsXyz2);
+                IList<PathOfTravel> routeMultiple = PathOfTravel.CreateMultiple(doc.ActiveView, roomsXyz1, roomsXyz2);
+
+                tx.Commit();
+            }
+            
+            return Result.Succeeded;
+        }
+    }
+}
